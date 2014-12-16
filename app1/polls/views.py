3f60ba69ad9fcff5de42c340fad89acbf5d1421a
thanhnq <thanhnq@Thanhs-MacBook-Air.local>
2014-12-16 07:18:28 +0700

    new

--- app1/polls/views.py
+++ app1/polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+#from django.template import RequestContext, loader
 
 from polls.models import Question
 
@@ -8,16 +9,24 @@ from polls.models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list, 
-    })
-    return HttpResponse(template.render(context))
+    #template = loader.get_template('polls/index.html')
+    #context = RequestContext(request, {
+    #    'latest_question_list': latest_question_list, 
+    #})
+    context = {'latest_question_list': latest_question_list}
+    #return HttpResponse(template.render(context))
+    return render(request, 'polls/index.html', context)
     #output = ', '.join([p.question_text for p in latest_question_list])
     #return HttpResponse(output)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #try:
+    #    question = Question.objects.get(id=question_id)
+    #except Question.DoesNotExist:
+    #    raise Http404
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    #return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
