4ecaa34d52ed1d197eaf46d019c5d4d86a6bd5e0
thanhnq <thanhnq@Thanhs-MacBook-Air.local>
2014-12-21 23:10:01 +0700

    commit

--- app1/polls/views.py
+++ app1/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 #from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
@@ -48,3 +49,18 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
     #return HttpResponse("You're voting on question %s." % question_id)
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
